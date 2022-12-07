bd2713682b7435aa04c068815a320cf9735b04d9
eman-debz <56546285+eman-debz@users.noreply.github.com>
2022-12-07 21:44:21 +0000

    Added tests for Header button click

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,15 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, getByLabelText, render, screen, waitFor } from "@testing-library/react";
 import App from './App';
 import Header from './components/Header';
 import '@testing-library/jest-dom';
 import {BrowserRouter} from 'react-router-dom'
 import AddTask from "./components/AddTask";
+import Button from "./components/Button";
 
 
 test('Render Task Tracker Header correctly', () => {
   render(<App/>);
+
   const h1Element = screen.getByText('Task Tracker');
   expect(h1Element).toBeInTheDocument();
 })
@@ -18,30 +20,48 @@ test('Render Task Tracker title as the Header', () => {
     <Header/>
   </BrowserRouter>
   );
+
   const headerEl = screen.getByTestId('header-title');
   expect(headerEl).toHaveTextContent('Task Tracker');
 })
 
-test('Header button text is Add Task', () => {
+test('Header button text is Add Task',  () => {
   render(
   <BrowserRouter>
     <Header/>
   </BrowserRouter>
   );
+
   const headerBtn = screen.getByTestId('header-button');
   expect(headerBtn).toHaveTextContent('Add Task');
 })
 
-test('Button registers click event', () => {
+test('On button click,expect button text to now equal: Close',async () => {
   render(<App/>);
-  const headerBtn = screen.getByTestId('header-button')
-  fireEvent.click(headerBtn)
-  const form = screen.getByTestId('add-task-form')
-  expect(form).toBeVisible();
+  
+  const headerBtn = screen.getByText('Add Task');
+  fireEvent.click(headerBtn);
+  await waitFor(() =>
+   expect(headerBtn).toHaveTextContent('Close')
+  );
+})
+
+test('On button click, expect Add Task form to be present', () => {
+  render(<App/>);
+
+  const headerBtn = screen.getByText('Add Task');
+  fireEvent.click(headerBtn);
+
+  
+  expect(screen.getByText('Task Name')).toBeInTheDocument();
+  expect(screen.getByText('Day & Time')).toBeInTheDocument();
+  expect(screen.getByText('Set Reminder')).toBeInTheDocument();
+
 })
 
 test('Value of the task input field is correct',() => {
   render(<AddTask/>);
+
   const taskInputValue = screen.getByTestId('task-value')
   fireEvent.change(taskInputValue,{
     target : {
@@ -54,6 +74,7 @@ test('Value of the task input field is correct',() => {
 
 test('Value of the day input field is correct',() => {
   render(<AddTask/>);
+
   const dayInputValue = screen.getByTestId('day-value')
   fireEvent.change(dayInputValue,{
     target : {
@@ -62,52 +83,55 @@ test('Value of the day input field is correct',() => {
   })
 
   expect(dayInputValue).toHaveValue('Monday 3rd October')
-
 })
 
 test('Value of reminder checkbox is correct',() => {
   render(<AddTask/>);
+
   const reminderInputValue = screen.getByTestId('day-value')
   fireEvent.change(reminderInputValue,{
     target : {
        checked : false
     }
   })
+
 expect(reminderInputValue).not.toBeChecked()
 })
 
-test('On click create alert for empty',() => {
+test('On click create alert for empty submission',() => {
   render(<AddTask/>);
+
   const alertMock = jest.spyOn(window,'alert').mockImplementation() 
   const submitBtn = screen.getByTestId('task-submit')
 
   fireEvent.click(submitBtn)
-
   expect(alertMock).toBeCalledTimes(1);
-
 })
 
-test('On submission, trigger submit handler',() => {
-  const submitTest = jest.fn();
-  render(<AddTask submitTest={submitTest}/>);
+
+test('On submission, trigger submit callback function',() => {
+  const submitTestFn = jest.fn();
+  const onAddFn = jest.fn();
+  render(<AddTask submitTest={submitTestFn} onAdd={onAddFn}/>);
   
   const taskInput = screen.getByTestId('task-value')
   const dayInput = screen.getByTestId('day-value')
   const reminderInput = screen.getByTestId('reminder-value')
   const submitBtn = screen.getByTestId('task-submit')
   
-  const form = screen.getByTestId('add-task-form')
 
   fireEvent.change(taskInput, {
     target : {
      value: 'Go to the shop'
     }
   })
+
   fireEvent.change(dayInput, {
     target : {
       value: 'Tuesday 5th December'
     }
   })
+
   fireEvent.change(reminderInput,{
     target : {
        checked : true
@@ -116,6 +140,5 @@ test('On submission, trigger submit handler',() => {
  
 fireEvent.click(submitBtn)
 
-expect(submitTest).toHaveBeenCalled()
-
+expect(submitTestFn).toHaveBeenCalled()
 })
